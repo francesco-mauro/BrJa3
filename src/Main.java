f8af4264b7fad045c46e8a9a3ad0653b4177e792
francesco-mauro <francesco.mauro180@gmail.com>
2025-08-09 19:36:17 +0200

    Br-3-4nd

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,53 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main (String [] args) {
 
-        int x = 10;
-        int y = 2;
-        int z = 10;
+//        int x = 10;
+//        int y = 2;
+//        int z = 10;
+//
+//        z = x + y;
+//        z = x - y;
+//        z = x * y;
+//        z = x / y;
+//
+//        System.out.println(z);
+//
+//        x += y;
+//        x++;
+//        x--;
+//
+//        System.out.print(x);
+//
+//        // Order od operation ParentesisExponentsMoltiplicationDivisionAdditionSubtraction
+//
+//        double result = 3 + 4 * (7 - 5) / 2.0;
+//
+//        System.out.println(result);
 
-        z = x + y;
-        z = x - y;
-        z = x * y;
-        z = x / y;
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(z);
+        String item;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total;
 
-        x += y;
-        x++;
-        x--;
+        System.out.println("What item do you want to buy?: ");
+        item = scanner.nextLine();
 
-        System.out.print(x);
+        System.out.println("What is the price for each?: ");
+        price = scanner.nextDouble();
 
-        // Order od operation ParentesisExponentsMoltiplicationDivisionAdditionSubtraction
+        System.out.println("how many would you like?: ");
+        quantity = scanner.nextInt();
 
-        double result = 3 + 4 * (7 - 5) / 2.0;
+        total = price * quantity;
+        System.out.println("\nYou have bought " +quantity+ " " + item + "/s");
+        System.out.println("Your total is " + currency + total);
 
-        System.out.println(result);
 
-
+        scanner.close();
     }
 }
