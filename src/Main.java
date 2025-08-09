c5d74048dccf5f6eafb126918c5fcf08c6ab3e31
francesco-mauro <francesco.mauro180@gmail.com>
2025-08-09 19:21:36 +0200

    Br-3-2nd

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main (String [] args) {
 
+        int x = 10;
+        int y = 2;
+        int z = 10;
+
+        z = x + y;
+        z = x - y;
+        z = x * y;
+        z = x / y;
+
+        System.out.println(z);
+
+        x += y;
+
+        System.out.print(x);
 
 
     }
